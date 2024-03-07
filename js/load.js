929c1aaffb1fdeadf4bd0361df06ef88594a440e
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-03-07 13:28:34 +0200

    added js folder and functions to load

---
+++ js/load.js
@@ -0,0 +1,6 @@
+// Use setTimeout to execute a function after a specified delay
+// this is a built in function in js
+setTimeout(function() {
+  // Change the browser's location (URL) to "home.html"
+  window.location.href = "home.html";
+}, 10000); //(10s) Wait for (1000 milliseconds (1 second)) before executing the function
