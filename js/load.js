b11a6ac835900a26825491121f1def0fe30e504d
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-03-07 13:50:03 +0200

    finalized loading page

--- js/load.js
+++ js/load.js
@@ -1,6 +1,22 @@
 // Use setTimeout to execute a function after a specified delay
 // this is a built in function in js
-setTimeout(function() {
-  // Change the browser's location (URL) to "home.html"
-  window.location.href = "home.html";
-}, 10000); //(10s) Wait for (1000 milliseconds (1 second)) before executing the function
+let countdown = 300;
+
+// Display the initial countdown
+updateCountdown();
+
+const countdownInterval = setInterval(function () {
+  countdown -= 1;
+  updateCountdown();
+
+  if (countdown <= 0) {
+    clearInterval(countdownInterval);
+    window.location.href = "home.html";
+  }
+}, 1000); // Update the countdown every 1000 milliseconds (1 second)
+
+function updateCountdown() {
+  // Update the countdown display on the page
+  const countdownDisplay = document.getElementById("countdownDisplay");
+  countdownDisplay.textContent = `Redirecting in ${countdown} seconds`;
+}
