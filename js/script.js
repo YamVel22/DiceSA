34b3904e52f1cacdea0cfcc48530231293cf5332
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-03-07 15:20:23 +0200

    updated js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,35 @@
-  // Variables to store player names
-  let player1Name = '';
-  let player2Name = '';
+// Function to submit player names
+// we are using local storage as we are goung to be calling this names later in another file 
+function submitPlayer(playerNumber) {
+  const playerName = document
+    .getElementById(`player${playerNumber}`)
+    .value.trim();
 
-  // Function to submit player names
-  function submitPlayer(playerNumber) {
-    const playerName = document.getElementById(`player${playerNumber}`).value.trim();
-
-    if (playerNumber === 1) {
-      player1Name = playerName;
-    } else if (playerNumber === 2) {
-      player2Name = playerName;
-    }
-
-    // Check if both names are entered
-    if (player1Name !== '' && player2Name !== '') {
-      document.getElementById('begin').disabled = false;
-    }
+  if (playerNumber === 1) {
+    localStorage.setItem("player1Name", playerName);
+  } else if (playerNumber === 2) {
+    localStorage.setItem("player2Name", playerName);
   }
 
-  // Function to begin the game
-  function beginGame() {
-    // Check if both names are entered before proceeding
-    if (player1Name !== '' && player2Name !== '') {
-      // Redirect to the next page or perform other actions
-      // Example: window.location.href = 'next_page.html';
-      console.log('Game is beginning!');
-    } else {
-      alert('Please enter both player names before beginning the game.');
-    }
+  // Check if both names are entered
+  if (
+    localStorage.getItem("player1Name") &&
+    localStorage.getItem("player2Name")
+  ) {
+    document.getElementById("continue").disabled = false;
   }
+}
+
+// Function to begin the game
+function continueGame() {
+  // Check if both names are entered before proceeding
+  const player1Name = localStorage.getItem("player1Name");
+  const player2Name = localStorage.getItem("player2Name");
+
+  if (player1Name && player2Name) {
+    console.log("Unto the Rules!");
+    window.location.href = "/html/rules.html";
+  } else {
+    alert("Please enter both player names before continuing the game.");
+  }
+}
