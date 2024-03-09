339060704a5b57e03a460b99a63010a021745782
Yamkelani Velaphi <106350341+YamVel22@users.noreply.github.com>
2024-03-09 03:03:06 +0200

    roll dice function modified

--- js/game.js
+++ js/game.js
@@ -7,6 +7,11 @@ cube2 = document.getElementById("cube2");
 var min = 1;
 var max = 24;
 
+var rollsPlayer1 = 0;
+var rollsPlayer2 = 0;
+var player1Score = 0;
+var player2Score = 0;
+
 dice1.onclick = function () {
   rollDice("player1", cube1);
 };
@@ -16,34 +21,54 @@ dice2.onclick = function () {
 };
 
 function rollDice(player, cube) {
-  var xRand = getRandom(max, min);
-  var yRand = getRandom(max, min);
+  if (player === "player1" && rollsPlayer1 < 2) {
+    rollsPlayer1++;
+    var xRand = getRandom(max, min);
+    var yRand = getRandom(max, min);
 
-  cube.style.webkitTransform =
-    "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
-  cube.style.transform = "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
+    cube.style.webkitTransform =
+      "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
+    cube.style.transform =
+      "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
 
-  var total = xRand / 90 + yRand / 90;
-  showResult(player, total);
+    var total = xRand / 90 + yRand / 90;
+    player1Score += total;
+    document.getElementById("player1Score").textContent =
+      "Score: " + player1Score;
+  }
+
+  if (player === "player2" && rollsPlayer2 < 2) {
+    rollsPlayer2++;
+    var xRand = getRandom(max, min);
+    var yRand = getRandom(max, min);
+
+    cube.style.webkitTransform =
+      "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
+    cube.style.transform =
+      "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
+
+    var total = xRand / 90 + yRand / 90;
+    player2Score += total;
+    document.getElementById("player2Score").textContent =
+      "Score: " + player2Score;
+  }
 }
 
-function showResult(player, total) {
-  var result = document.getElementById("result");
-  var currentPlayer = document
-    .getElementById(player)
-    .getElementsByTagName("h2")[0].textContent;
+document.getElementById("calculate").onclick = function () {
+  calculateScore();
+};
 
-  result.textContent = currentPlayer + " scored " + total + " points.";
+function calculateScore() {
+  if (rollsPlayer1 === 2 && rollsPlayer2 === 2) {
+    var result = document.getElementById("result");
 
-  // Compare scores to determine the winner
-  var previousScore = parseFloat(result.dataset.score);
-  if (isNaN(previousScore) || total > previousScore) {
-    result.dataset.score = total;
-    result.textContent += " You are the current leader!";
-  } else if (total === previousScore) {
-    result.textContent += " It's a tie!";
-  } else {
-    result.textContent += " Maybe next time!";
+    if (player1Score > player2Score) {
+      result.textContent = "Player 1 is the winner!";
+    } else if (player2Score > player1Score) {
+      result.textContent = "Player 2 is the winner!";
+    } else {
+      result.textContent = "It's a tie!";
+    }
   }
 }
 
