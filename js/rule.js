8875d4b0651062221d1b7e6484f6c4e7ebb7b708
Yamkelani Velaphi <106350341+YamVel22@users.noreply.github.com>
2024-03-09 03:13:54 +0200

    added function to link to game page

--- js/rule.js
+++ js/rule.js
@@ -7,3 +7,7 @@ function goBack() {
   // window.location.href = "https://youtu.be/QtXby3twMmI?si=a7y9XcFHzHfwpcqA";
   window.location.href = "/html/player.html";
 }
+
+function beginGame() {
+  window.location.href = "/html/game.html";
+}
