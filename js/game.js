fa37bbe84ff218fa3ab4ef283ce33b094ce06862
Yamkelani Velaphi <106350341+YamVel22@users.noreply.github.com>
2024-03-09 12:23:16 +0200

    dice is filly rolling

--- js/game.js
+++ js/game.js
@@ -1,10 +1,51 @@
-//how do I call those local files for the names?//
+
+//get the players names from the player pages//
+
 
 // Get elements
+let cube = document.getElementById("cube1");
 let cube1 = document.getElementById("cube2");
 let dice1 = document.getElementById("dice1");
 let dice2 = document.getElementById("dice2");
 let results = document.getElementById("results");
 
-var min = 1;
-var max = 24;
+let min = 1;
+let max = 24;
+
+//Player 1's dice roll
+dice1.onclick = function () {
+  let xRand = getRandom(max, min);
+  let yRand = getRandom(max, min);
+
+  cube.style.webkitTranform =
+    "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
+  cube.style.transform = "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
+
+  xRand = getRandom(max, min);
+  yRand = getRandom(max, min);
+
+  cube2.style.webkitTransform =
+    "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
+  cube2.style.transform = "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
+};
+
+//Player 2's dicce roll
+dice2.onclick = function () {
+  let xRand = getRandom(max, min);
+  let yRand = getRandom(max, min);
+
+  cube.style.webkitTranform =
+    "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
+  cube.style.transform = "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
+
+  xRand = getRandom(max, min);
+  yRand = getRandom(max, min);
+
+  cube2.style.webkitTransform =
+    "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
+  cube2.style.transform = "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
+};
+
+function getRandom(max, min) {
+  return (Math.floor(Math.random() * (max - min)) + min) * 90;
+}
