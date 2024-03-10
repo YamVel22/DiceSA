b5273ebb0844f4f8a9da2ed36fe2307309ef6191
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-03-10 16:49:56 +0200

    updated dice settings

--- js/game.js
+++ js/game.js
@@ -1,6 +1,33 @@
 const dice1 = document.querySelector("#dice1");
 const dice2 = document.querySelector("#dice2");
 
+const player1 = {
+  name: localStorage.getItem("player1Name") || "Player One",
+  score: 0,
+  number: 1,
+};
+
+const player2 = {
+  name: localStorage.getItem("player2Name") || "Player Two",
+  score: 0,
+  number: 2,
+};
+
+// Function to update the player names on the HTML page
+function updatePlayerNames() {
+  document.getElementById("name1").innerText = player1.name;
+  document.getElementById("name2").innerText = player2.name;
+}
+
+// Call the function to update player names initially
+updatePlayerNames();
+
+// Add event listener to the button for rolling the dice
+document.querySelector("button").addEventListener("click", function () {
+  currentFace1 = updateDice(dice1, currentFace1, player1);
+  currentFace2 = updateDice(dice2, currentFace2, player2);
+});
+
 function showFace(face) {
   face.classList.add("fadeIn");
   setTimeout(function () {
@@ -29,12 +56,16 @@ function generateNum() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
-function updateDice(diceElement, currentFace) {
+function updateDice(diceElement, currentFace, player) {
   hideFace(currentFace);
   rollDice(diceElement);
-  const num = generateNum();
 
-  switch (num) {
+  const num1 = generateNum();
+
+  // Ensure a 10% probability of both dice being the same number
+  const num2 = Math.random() < 0.1 ? num1 : generateNum();
+
+  switch (num1) {
     case 1:
       currentFace = diceElement.querySelector(".dot");
       break;
@@ -58,18 +89,16 @@ function updateDice(diceElement, currentFace) {
   showFace(currentFace);
   setTimeout(function () {
     stopRoll(diceElement);
+    updateScore(player, num1 + num2);
   }, 3000);
 
   return currentFace;
 }
 
-dice1.addEventListener("click", function () {
-  currentFace1 = updateDice(dice1, currentFace1);
-});
-
-dice2.addEventListener("click", function () {
-  currentFace2 = updateDice(dice2, currentFace2);
-});
+function updateScore(player, num) {
+  player.score += num;
+  document.getElementById(`score${player.number}`).innerText = player.score;
+}
 
 // Initialize currentFace variables
 let currentFace1 = dice1.querySelector(".six");
